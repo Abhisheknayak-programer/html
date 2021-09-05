07754eac696eb28e8b71ec5f26b57bb3c7e722d6
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2021-09-05 16:21:22 +0530

    Final  View  Without Media Queries Only for desktop

--- Consultancy page/script.js
+++ Consultancy page/script.js
@@ -4,10 +4,104 @@
 const navLinks = document.querySelectorAll(".nav__links");
 const dropDownNav = document.querySelector(".dropDown");
 navLinks.forEach((Element) =>
-  Element.addEventListener("mousedown", () => {
-    dropDownNav.classList.toggle("navDisabled");
+  Element.addEventListener("mouseover", () => {
+    dropDownNav.classList.remove("navDisabled");
   })
 );
+
+navLinks.forEach((Element) => {
+  Element.addEventListener("mouseout", function () {
+    dropDownNav.classList.add("navDisabled");
+  });
+});
+const dropDowncol1 = document.querySelector(".dropDown__col__1");
+const dropDowncol2 = document.querySelector(".dropDown__col__2");
+const dropDowncol3 = document.querySelector(".dropDown__col__3");
+
+const goOut = function () {
+  dropDowncol2.style.opacity = "0";
+  dropDowncol3.style.opacity = "0";
+};
+
+dropDowncol1.addEventListener("mouseover", () => {
+  dropDowncol2.style.opacity = "1";
+});
+
+dropDowncol2.addEventListener("mouseover", () => {
+  dropDowncol2.style.opacity = "1";
+  dropDowncol3.style.opacity = "1";
+});
+
+dropDowncol3.addEventListener("mouseover", () => {
+  dropDowncol2.style.opacity = "1";
+  dropDowncol3.style.opacity = "1";
+});
+
+dropDownNav.addEventListener("mouseover", () => {
+  dropDownNav.classList.remove("navDisabled");
+});
+
+dropDownNav.addEventListener("mouseout", () => {
+  dropDownNav.classList.add("navDisabled");
+  goOut();
+});
+
+const navLinkManipulate_1 = document.querySelector("#nav__link_1");
+const navLinkManipulate_2 = document.querySelector("#nav__link_2");
+const navLinkManipulate_3 = document.querySelector("#nav__link_3");
+const navLinkManipulate_4 = document.querySelector("#nav__link_4");
+const navLinkManipulate_5 = document.querySelector("#nav__link_5");
+const navLinkManipulate_6 = document.querySelector("#nav__link_6");
+const ManipulatedLInks = document.querySelectorAll(".ManipulatedLInks");
+const colOneOpen = document.querySelector("#colOneOpen");
+const colTwoOpen = document.querySelector("#colTwoOpen");
+
+navLinkManipulate_1.addEventListener("mouseover", () => {
+  ManipulatedLInks.forEach(
+    (Element) => (Element.textContent = "Random Link 1")
+  );
+});
+
+navLinkManipulate_2.addEventListener("mouseover", () => {
+  ManipulatedLInks.forEach(
+    (Element) => (Element.textContent = "Random Link 2")
+  );
+});
+
+navLinkManipulate_3.addEventListener("mouseover", () => {
+  ManipulatedLInks.forEach(
+    (Element) => (Element.textContent = "Random Link 3")
+  );
+});
+
+navLinkManipulate_4.addEventListener("mouseover", () => {
+  ManipulatedLInks.forEach(
+    (Element) => (Element.textContent = "Random Link 4")
+  );
+});
+
+navLinkManipulate_5.addEventListener("mouseover", () => {
+  ManipulatedLInks.forEach(
+    (Element) => (Element.textContent = "Random Link 5")
+  );
+});
+
+navLinkManipulate_6.addEventListener("mouseover", () => {
+  ManipulatedLInks.forEach(
+    (Element) => (Element.textContent = "Random Link 6")
+  );
+});
+
+dropDowncol2.style.opacity = "0";
+dropDowncol3.style.opacity = "0";
+
+colOneOpen.addEventListener("mouseover", () => {
+  dropDowncol2.style.opacity = "1";
+});
+
+colTwoOpen.addEventListener("mouseover", () => {
+  dropDowncol3.style.opacity = "1";
+});
 
 ////////////////////////
 // IMAGE MANIPULATOR //
