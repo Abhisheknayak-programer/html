b0d975bcbecebfb525e98a2c301206fe549a1f34
ABHISHEK NAYAK <66876321+Abhisheknayak-programer@users.noreply.github.com>
2022-09-05 04:37:16 +0530

    Delete db directory

--- db/conn.js
+++
@@ -1,7 +0,0 @@
-// Mongodb Connection
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Artow",{useCreateIndex:true,useNewUrlParser:true,useFindAndModify:false,useUnifiedTopology:true}).then(()=>{
-    console.log("We Are Connected To DataBase")
-}).catch((err)=>{
-    console.log(err);
-})
